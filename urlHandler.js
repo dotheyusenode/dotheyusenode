2b740cff58391cf5601f32d3411f5220db1903e9
wlaurance <w.laurance@gmail.com>
2014-02-06 11:47:52 -0500

    Alert out the status

--- urlHandler.js
+++ urlHandler.js
@@ -1,5 +1,10 @@
+var isValid = require('./isValid');
+
 module.exports = function(req, res) {
   var url = req.query.url;
-  console.log(url);
-  res.send(200, {url: url});
+  if (isValid(url)) {
+    res.send(200, {message: url + ' is valid'});
+  } else {
+    res.send(400, {error: url + ' is not a valid url'});
+  }
 };
