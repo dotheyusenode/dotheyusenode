5b0d2b2ccd600f05e5261220f4a8f5e55cf48f0e
wlaurance <w.laurance@gmail.com>
2014-02-06 16:42:07 -0500

    Express IN header

--- checkers/express.js
+++ checkers/express.js
@@ -7,7 +7,7 @@ module.exports = function(res, body, cb) {
   var headers = res.headers;
   if (headers['x-powered-by'] && headers['x-powered-by'].toLowerCase() === 'express') {
     checker.found = true;
-    checker.reasons.push('Found X-Powered-By: Express header is response');
+    checker.reasons.push('Found X-Powered-By: Express header in response');
   }
   cb(null, checker);
 };
