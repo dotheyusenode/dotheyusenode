0b728ba64dcd12946a414d48dc2541f07848d45f
wlaurance <w.laurance@gmail.com>
2014-02-11 15:39:28 -0500

    Clean up coffeescript checker output

--- checkers/needsjs/coffeescript.js
+++ checkers/needsjs/coffeescript.js
@@ -1,31 +1,35 @@
-var cfdetector = require('coffeescript-detector');
-var async = require('async');
-var _ = require('underscore');
+var cfdetector = require('coffeescript-detector')
+var async = require('async')
+var _ = require('underscore')
 
 function handler(obj, cb) {
-  var body = obj.body;
-  var request = obj.request;
-  var pairs = obj.pairs;
+  var pairs = obj.pairs
   async.map(pairs, function(p, cb) {
     cfdetector(p[1], function(err, results) {
       if (results) {
-        p.push(results);
+        p.push(results)
       }
-      cb(err, p);
-    });
+      cb(err, p)
+    })
   }, function(err, mapped){
-    if (err) return cb(err);
-    var reasons = _.filter(mapped, function(a) { return a.length === 3; });
-    if (reasons.length > 0) {
+    if (err) {
+      return cb(err)
+    }
+    var positives = _.filter(mapped, function(a) { return a.length === 3 })
+    var reasons = _.map(positives, function(p) {
+      return p[0] + ": " + JSON.stringify( p[2].lines)
+    })
+
+    if (positives.length > 0) {
       cb(null, {
         name: 'CoffeeScript',
         found: true,
         reasons: reasons
-      });
+      })
     } else {
-      cb(null);
+      cb(null)
     }
-  });
+  })
 }
 
-module.exports = handler;
+module.exports = handler
