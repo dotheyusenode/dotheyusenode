bc44a9e5121840dfd9b60d08e0787b8845eef4bf
wlaurance <w.laurance@gmail.com>
2014-02-10 22:43:18 -0500

    Use the coffeescript detector
    WIP returning results

--- checkers/needsjs/coffeescript.js
+++ checkers/needsjs/coffeescript.js
@@ -1,5 +1,18 @@
-function handler(r, body, dom, cb) {
+var cfdetector = require('coffeescript-detector');
+var async = require('async');
 
+function handler(obj, cb) {
+  var body = obj.body;
+  var request = obj.request;
+  var pairs = obj.pairs;
+  async.map(pairs, function(p, cb) {
+    cfdetector(p[0], function(err, results) {
+      p.push(results);
+      cb(err, p);
+    });
+  }, function(err, mapped){
+    if (err) return cb(err);
+  });
 }
 
 module.exports = handler;
--- checkers/needsjs/index.js
+++ checkers/needsjs/index.js
@@ -5,20 +5,26 @@ var request = require('request');
 var async = require('async');
 
 function handler(r, b, cb) {
-  var $ = cheerio.load(b);  
+  var $ = cheerio.load(b);
   var scripts = _.filter(
     _.pluck(_.pluck($('script'), 'attribs'), 'src'),
     function(e) { return e;});
   async.map(scripts, request, function(err, results) {
-    function examine(e, cbe) {
-      cbe(null);
+    function examine(pairs) {
+      return function(e, cbe) {
+        e({
+          request: r,
+          body: b,
+          pairs: pairs
+        }, cbe);
+      };
     }
     if (err) {
       return cb(err);
     } else {
       var pairs = _.zip(scripts, results);
       var examiners = [coffeescript];
-      async.map(examiners, examine, cb);
+      async.map(examiners, examine(pairs), cb);
     }
   });
 }
