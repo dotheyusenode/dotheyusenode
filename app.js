c277a39b149054333c505821a875522b89c9fcd2
meredithmmyers <myers.mm89@gmail.com>
2014-02-19 17:05:55 -0500

    Add Angular dependencies and display default angular-seed app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 // development only
 if ('development' === app.get('env')) {
