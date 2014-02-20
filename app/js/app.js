9839dcc2ad531dcb24ed4734efc1947b20e74aff
meredithmmyers <myers.mm89@gmail.com>
2014-02-19 20:05:13 -0500

    Move html to partial, set up router

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
@@ -10,7 +10,12 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.
+    when('/', {
+      templateUrl: 'partials/partial.html', 
+      controller: 'AppCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,6 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
+  controller('AppCtrl', [function() {
 
   }]);
