48570f161dd24faedb75fdcf8931fe119b4f2a86
meredithmmyers <myers.mm89@gmail.com>
2014-02-20 21:19:40 -0500

    Make GET request on click. Currently throws an error

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,31 @@ angular.module('myApp.controllers', []).
   controller('AppCtrl', function($scope) {
     $scope.website = {url: ''};
     $scope.check = function() {
-      console.log($scope.website);
+      var url = $scope.website.url;
+      if (url !== "") {
+        $scope.makeRequest(url, $scope.handleRequest);
+      }
+    }
+    $scope.handleRequest = function(code, body) {
+      writeTitle();
+
+      if (code === 200) {
+        writeMessage(body.message);
+      } else {
+        writeError(body.error);
+      }
+    }
+    $scope.makeRequest = function(url, cb) {
+      console.log(url);
+      var http = new XMLHttpRequest();
+      console.log(encodeURIComponent(url))
+      http.open("GET", "/?url=" + encodeURIComponent(url), true);
+      http.onreadystatechange = function() {
+        if (http.readyState == 4) {
+          console.log(http.responseText)
+          cb(http.status, JSON.parse(http.responseText));
+        }
+      };
+      http.send(null);
     }
   });
