ac6f0872565a00b022d04163c3e93b394a4ab1ab
meredithmmyers <myers.mm89@gmail.com>
2014-02-21 10:59:19 -0500

    Display result of request & return request URL with result

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,20 +5,28 @@
 angular.module('myApp.controllers', []).
   controller('AppCtrl', function($scope, $http) {
     $scope.website = {url: ''};
+    $scope.displayResult = false;
+    $scope.displayError = false;
     $scope.check = function() {
       var url = $scope.website.url;
       if (url !== "") {
+        $scope.reasons = [];
         $scope.makeRequest(url);
       }
     }
     $scope.makeRequest = function(url) {
       $http({method: 'GET', url: "/?url=" +encodeURIComponent(url)}).
         success(function(data, status, headers, config) {
-          console.log(status);
-          console.log(data);
+          $scope.displayResult = true;
+          $scope.displayError = false;
+          $scope.resultUrl = data.message.url;
+          $scope.message = data.message.answer;
+          $scope.reasons = data.message.reasons[0].reasons;
         }).
         error(function(data, status, headers, config) {
-          console.log(status);
+          $scope.displayError = true;
+          $scope.displayResult = false;
+          $scope.errorMsg = "Sorry, an error "+status+" occured";
         });
     }
   });
