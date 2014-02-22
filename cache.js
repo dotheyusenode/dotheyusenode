dd3ed049d7cb76d1dc21cd809abe635c196818a4
wlaurance <w.laurance@gmail.com>
2014-02-21 21:59:53 -0500

    Use the cache to retrieve keys

--- cache.js
+++ cache.js
@@ -3,7 +3,8 @@ var red = require('./red')
 
 var cache = src({
   url: red,
-  expiry: 60 * 20 //20 minutes
+  expiry: 60 * 20, //20 minutes
+  id: process.env.SRC_ID || 'dev'
 })
 
 module.exports = cache
